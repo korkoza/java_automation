338f620cedf590942b6613d39bfec09e8667fe73
Mykola Korkoza <mykola.korkoza@eleks.com>
2021-03-23 22:17:19 +0200

    ''

---
+++ src/exceptions/ExceptionPractice.java
@@ -0,0 +1,28 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class ExceptionPractice {
+    public static void throwIOException() throws IOException {
+        throw new IOException();
+    }
+
+    public static void throwIOExceptionWithFinally() {
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            System.out.println("IOException has been caught");
+        } finally {
+            System.out.println("This message will be printed anyway. It's placed in the finally block");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            throwIOException();
+        } catch (IOException e) {
+            System.out.println("IOException has been caught");
+        }
+        throwIOExceptionWithFinally();
+    }
+}
