f071050dbd8c12336d27df36fff9bcecefebb95a
mykola.korkoza <mykola.korkoza@gmail.com>
2021-05-10 22:48:24 +0300

    added allure steps

--- src/main/page_objects/CategoryPage.java
+++ src/main/page_objects/CategoryPage.java
@@ -2,6 +2,7 @@ package page_objects;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,6 +18,7 @@ public class CategoryPage {
         }
     }
 
+    @Step("Opened Plants subcategory")
     public SubCategoryPage openPlantsSubCategory() {
         $x("//a[@title='Рослини і догляд за ними'][1]")
                 .scrollTo()
@@ -24,6 +26,7 @@ public class CategoryPage {
         return Selenide.page(SubCategoryPage.class);
     }
 
+    @Step("Opened Watering Cans subcategory")
     public SubCategoryPage openWateringCanSubCategory() {
         $x("//a[@title='Лійки для квітів']")
                 .scrollTo()
@@ -31,6 +34,7 @@ public class CategoryPage {
         return Selenide.page(SubCategoryPage.class);
     }
 
+    @Step("Opened Notebooks subcategory")
     public SubCategoryPage openNotebookSubcategory() {
         $x("//a[@title='Ноутбуки'][2]")
                 .scrollTo()
